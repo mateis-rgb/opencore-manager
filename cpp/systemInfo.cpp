7768889c3d531c6b53cf4bdec52d4b76da3f0743
mat <54773138+mateis-rgb@users.noreply.github.com>
2023-11-28 12:58:36 +0000

    new file:   cpp/systemInfo.cpp

---
+++ cpp/systemInfo.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+
+class System
+{
+	private:
+		std::string cpu;
+		std::string gpu;
+		std::string lan;
+		std::string wan;
+		std::string storage;
+
+    void setCpu ()
+    {
+		this->cpu = system("grep -m 1 'model name' /proc/cpuinfo");
+    }
+
+	void setGpu ()
+	{
+		this->cpu = system("")
+	}
+
+	std::string getCpu ()
+	{
+		return this->cpu;
+	}
+
+	std::string getGpu ()
+	{
+		return this->gpu;
+	}
+
+	std::string getLan ()
+	{
+		return this->lan;
+	}
+
+	std::string getWan ()
+	{
+		return this->wan;
+	}
+
+	std::string getStorage ()
+	{
+		return this->storage;
+	}
+};
